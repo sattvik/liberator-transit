b215b12d33ed6d31d7d75e9bfc3c46683bf482eb
Daniel Solano Gómez <daniel@solanogomez.org>
2014-08-10 12:50:12 -0500

    Version 0.3.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.clojure/liberator-transit "0.2.0"
+(defproject io.clojure/liberator-transit "0.3.0-SNAPSHOT"
   :description "Library to add Transit encoding support to Liberator"
   :url "https://github.com/sattvik/liberator-transit"
   :license {:name "Eclipse Public License"
