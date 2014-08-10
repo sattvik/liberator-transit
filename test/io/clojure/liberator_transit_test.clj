37d69abfe913dfcffc835ad1b1a9b6c305648b9c
Daniel Solano Gómez <daniel@solanogomez.org>
2014-08-09 20:10:20 -0500

    Move generators into their own namespace

--- test/io/clojure/liberator_transit_test.clj
+++ test/io/clojure/liberator_transit_test.clj
@@ -8,14 +8,13 @@
 ; license.  You must not remove this notice, or any other, from this software.
 
 (ns io.clojure.liberator-transit-test
-  (:require [clojure.data.generators :as data-gen]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
             [clojure.test :refer :all]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
             [cognitect.transit :as transit]
             [io.clojure.liberator-transit]
+            [io.clojure.liberator-transit.generators :as gen]
             [liberator.core :refer [defresource]]
             [liberator.dev :refer [wrap-trace]]
             [ring.mock.request :as mock]))
@@ -116,118 +115,16 @@
            [0x81 0xa3 0x66 0x6f 0x6f 0x01] {"foo" 1}
            [0x82 0xa3 0x7e 0x3a 0x61 0x80 0xa3 0x7e 0x3a 0x62 0x2a] {:a {} :b 42}))))
 
-(def nil-generator
-  (gen/return nil))
-
-(def normal-float-generator
-  (gen/make-gen
-    (fn [^java.util.Random rnd size]
-      (loop []
-        (let [value (Float/intBitsToFloat (.nextInt rnd))]
-          (if (or (.isNaN value) (.isInfinite value))
-            (recur)
-            [value []]))))))
-
-(def float-generator
-  (gen/frequency [[17 normal-float-generator]
-                  [1 (gen/return Float/NEGATIVE_INFINITY)]
-                  [1 (gen/return Float/POSITIVE_INFINITY)]
-                  [1 (gen/return Float/NaN)]]))
-
-(def normal-double-generator
-  (gen/make-gen
-    (fn [^java.util.Random rnd _]
-      (loop []
-        (let [value (Double/longBitsToDouble (.nextLong rnd))]
-          (if (or (.isNaN value) (.isInfinite value))
-            (recur)
-            [value []]))))))
-
-(def double-generator
-  (gen/frequency [[17 normal-double-generator]
-                  [1 (gen/return Double/NEGATIVE_INFINITY)]
-                  [1 (gen/return Double/POSITIVE_INFINITY)]
-                  [1 (gen/return Double/NaN)]]))
-
-(def big-decimal-generator
-  (gen/make-gen
-    (fn [^java.util.Random rnd size]
-      (binding [data-gen/*rnd* rnd]
-        [(data-gen/bigdec) []]))))
-
-(def big-int-generator
-  (gen/make-gen
-    (fn [^java.util.Random rnd size]
-      (binding [data-gen/*rnd* rnd]
-        [(data-gen/bigint) []]))))
-
-
-(def time-generator
-  (gen/make-gen
-    (fn [^java.util.Random rnd size]
-      (binding [data-gen/*rnd* rnd]
-        [(data-gen/date) []]))))
-
-(def uuid-generator
-  (gen/make-gen
-    (fn [^java.util.Random rnd size]
-      (binding [data-gen/*rnd* rnd]
-        [(data-gen/uuid) []]))))
-
-(def uri-scheme-generator
-  (gen/one-of [nil-generator (gen/fmap clojure.string/join (gen/not-empty (gen/vector gen/char-alpha)))]))
-
-; TODO: really generate URIs
-(def uri-generator
-  (gen/elements [(java.net.URI. "/foo/bar/baz")
-                 (java.net.URI. "http://example.com")
-                 (java.net.URI. "https://user:foo@example.com/baz/dldl/lsjdlj?q=42")
-                 (java.net.URI. "https://user:foo@example.com/baz/dldl/lsjdlj?q=42#ldjd")
-                 (java.net.URI. "urn:bar")
-                 (java.net.URI. "file:foo")
-                 (java.net.URI. "file:/foo")
-                 (java.net.URI. "http://localhost:9000/check")
-                 (java.net.URI. "http://[::1]:9000/check")]))
-
-(def simple-type-generator
-  (gen/frequency [[10 gen/simple-type]
-                  [1 nil-generator]
-                  [1 double-generator]
-                  [1 float-generator]
-                  [1 big-decimal-generator]
-                  [1 big-int-generator]
-                  [1 time-generator]
-                  [1 uuid-generator]
-                  [1 uri-generator]]))
-
-(defn set-generator
-  [inner-type]
-  (gen/fmap set (gen/vector inner-type)))
-
-(defn container-generator
-  [inner-type]
-  (gen/one-of [(gen/vector inner-type)
-               (gen/list inner-type)
-               (gen/map inner-type inner-type)
-               (set-generator inner-type)]))
-
-(def content-generator
-  (gen/recursive-gen container-generator simple-type-generator))
-
-(def sequence-generator
-  (gen/one-of [(gen/vector content-generator)
-               (gen/list content-generator)]))
-
 (defspec generated-sequences
   80
-  (prop/for-all [v sequence-generator]
+  (prop/for-all [v gen/sequence-generator]
     (= (jsonify v) (to-string ((test-resource v) (json-request))))
     (= (jsonify v :verbose) (to-string ((test-resource v) (json-request :verbose))))
     (= (packify v) (to-bytes ((test-resource v) (msgpack-request))))))
 
 (defspec generated-maps
   80
-  (prop/for-all [v (gen/map simple-type-generator content-generator)]
+  (prop/for-all [v gen/map-generator]
     (= (jsonify v) (to-string ((test-resource v) (json-request))))
     (= (jsonify v :verbose) (to-string ((test-resource v) (json-request :verbose))))
     (= (packify v) (to-bytes ((test-resource v) (msgpack-request))))))
